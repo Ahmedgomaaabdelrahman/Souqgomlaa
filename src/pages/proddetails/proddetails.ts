5aaf46ffa260683f7cc0c152e62ddadeb7458718
mohamed essam <mohamed.essam9393@gmail.com>
2017-12-18 13:40:53 +0200

    k

--- src/pages/proddetails/proddetails.ts
+++ src/pages/proddetails/proddetails.ts
@@ -1,3 +1,4 @@
+import { LoginPage } from './../login/login';
 import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
@@ -9,6 +10,7 @@ import {DomainProvider} from "../../providers/domain/domain";
 import {FavProvider} from "../../providers/fav/fav";
 import {MessagedetailsPage} from "../messagedetails/messagedetails";
 import {ChatProvider} from "../../providers/chat/chat";
+import { Events } from 'ionic-angular';
 
 @Component({
   selector: 'page-proddetails',
@@ -19,11 +21,14 @@ Name=this.navParams.data.SellerId.Name;
 image=this.navParams.data.SellerId.image;
 D:any;
 star:string;
-  constructor(public chat :ChatProvider,public favProvider:FavProvider,public domain:DomainProvider,public itemsProvider:ItemsProvider,public common:CommonProvider,public navCtrl: NavController, public navParams: NavParams) {
+guestUser
+  constructor(public events :Events,public chat :ChatProvider,public favProvider:FavProvider,public domain:DomainProvider,public itemsProvider:ItemsProvider,public common:CommonProvider,public navCtrl: NavController, public navParams: NavParams) {
       console.log('ionViewDidLoad ProddetailsPage',this.navParams.data.SellerId.Name);
       // this.Name=this.navParams.data.SellerId.Name
       this.D=this.domain.url;
-
+  this.events.subscribe('guest',guest=>{
+         this.guestUser=guest;
+        })
 
   }
 res:any=[];
@@ -34,6 +39,8 @@ res:any=[];
     this.res=this.navParams.data;
     let self=this;
       this.common.getStoredValue('S').then(res=>{
+                        if(res != null){
+
           console.log('fav',res.Id,this.navParams.data.Id);
 
           this.favProvider.checkFav(res.Id,this.navParams.data.Id).subscribe(res=>{
@@ -41,7 +48,7 @@ res:any=[];
           this.star=res;
               console.log('fav',this.star);
 
-      });
+      });}
       });
       this.itemsProvider.getItemsImgsUrls(this.res.Id).then(res=>{
 
@@ -60,12 +67,15 @@ res:any=[];
 
     favTogle(){
         this.common.getStoredValue('S').then(res=>{
-
+                if(res!= null){
             this.favProvider.addToFavorites(res.Id,this.navParams.data.Id).subscribe(res=>{
             console.log('fav',this.star);
             // self.currentId=res.Id;
             this.star=res;
-        });
+        });}else{
+          this.common.presentToast(' عفوا يجب التسجيل اولا','اغلاق')
+          this.navCtrl.push(LoginPage)
+        }
         });
       // this.star=star;
 
@@ -74,6 +84,8 @@ res:any=[];
         console.log(this.navParams.data.SellerId)
 
         this.common.getStoredValue('S').then(user=>{
+                if(user != null){
+
           console.log('data passed to chat',user.Id,this.navParams.data.SellerId.sellerId)
         this.chat.getMessagesForDetailsPagetodetailsmessages(user.Id,this.navParams.data.SellerId.sellerId).then(res=>{
           console.log('data passed to chat',res)
@@ -88,6 +100,10 @@ res:any=[];
 
 // }
         })
+      }else{
+          this.common.presentToast(' عفوا يجب التسجيل اولا','اغلاق')
+          this.navCtrl.push(LoginPage)
+        }
         })
     }
     mainPage(){
