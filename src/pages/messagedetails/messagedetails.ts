f5f101f3e65db35ae38f4f8b930d1890f01e8a8b
HeshamEG <heshamsalama1212901022@gmail.com>
2018-01-15 10:38:24 +0200

    Revert "k"
    This reverts commit 1ebdefcb604c18cfdc8ab0106d06fc91ddbc79ae.

--- src/pages/messagedetails/messagedetails.ts
+++ src/pages/messagedetails/messagedetails.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild ,NgZone} from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import {Content, Events, Keyboard, NavController, NavParams} from 'ionic-angular';
 import {ChatProvider} from "../../providers/chat/chat";
 import {CommonProvider} from "../../providers/common/common";
@@ -21,16 +21,8 @@ myId:any;
 scrollContent:any;
 ref=firebase.database().ref();
 
-  constructor(public ngZone:NgZone,public event:Events,public keyboard: Keyboard,public common:CommonProvider,public chat:ChatProvider,public navCtrl: NavController, public navParams: NavParams) {
-      
-
-  }
-
-
-
-
-  ionViewWillEnter(){
-// this.scrollContent=this.content['_scroll']['initialized']
+  constructor(public event:Events,public keyboard: Keyboard,public common:CommonProvider,public chat:ChatProvider,public navCtrl: NavController, public navParams: NavParams) {
+      // this.scrollContent=this.content['_scroll']['initialized']
       this.common.getStoredValue('S').then(user=>{
           this.myId=user.Id
           // this.content.scrollToBottom();
@@ -45,7 +37,7 @@ this.icon="ios-send"
     if(this.navParams.data.instances.key!=null){
       this.ref.child('msgs/'+this.navParams.data.instances.key).on('value', data =>{
 
-          this.chat.allmsgs(this.navParams.data.instances.key).then(msgs=>{this.ngZone.run(() => {
+          this.chat.allmsgs(this.navParams.data.instances.key).then(msgs=>{
     console.log(msgs)
     self.myMsgs=[]
     for(var i in msgs){
@@ -54,7 +46,7 @@ this.icon="ios-send"
 
     }
               // this.content.scrollToBottom(0)
-})
+
 
 })
  setTimeout(() => {
@@ -67,6 +59,14 @@ this.icon="ios-send"
 //////chat//////
 // if(this.keyboard.didShow)this.icon="ios-send"
 //       else if(this.keyboard.didHide)this.icon="ios-send"
+
+
+  }
+
+
+
+
+  ionViewDidLoad(){
 
       console.log( this.myMsgs)
 
